fa548cd0923864f89f93d4f95a9eac50aa0e2192
OsandaLelum <41205668+OsandaLelum@users.noreply.github.com>
2020-07-04 11:25:58 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import Menu from './components/Menu'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Content from './components/Content'
+
+
+export default function App() {
+  return (
+    <div>
+       <Header/>
+      <Menu/>
+     
+ 
+      <Content/>
+      <Footer/>
+     
+   
+      
+    </div>
+  )
+}
---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+     <footer className="main-footer">
+      <strong>2020 Job Partner <a href="https://www.facebook.com/Work4Me-113602957079588/">Work4Me</a>.</strong>
+         All rights reserved.
+          <div className="float-right d-none d-sm-inline-block">
+          <b>Version</b> 3.0.4
+  </div>
+</footer>
+
+    )
+}
