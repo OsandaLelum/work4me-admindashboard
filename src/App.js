bec852115e2f97078d7e4d4c725d82d7100c36cf
OsandaLelum <41205668+OsandaLelum@users.noreply.github.com>
2020-07-05 13:34:15 +0530

    add  signiin for the admin dashboard

--- src/App.js
+++ src/App.js
@@ -4,19 +4,21 @@ import Menu from './components/Menu'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Content from './components/Content'
+//import SignIn from './components/Auth/SignIn'
+
+
+
 
 
 export default function App() {
   return (
     <div>
-       <Header/>
-      <Menu/>
-     
  
-      <Content/>
-      <Footer/>
+     <Header/>
+     <Menu/>
+     <Content/>
+     <Footer/>
      
-   
       
     </div>
   )
