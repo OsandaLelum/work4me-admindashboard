582c0eaf19eec016d2394bf7ebc0a317062ad69c
Christina Shelani <christinashelani@gmail.com>
2020-08-25 14:11:12 +0530

    Modifications in pages

--- src/components/Pages/AdminProfile.js
+++ src/components/Pages/AdminProfile.js
@@ -1,13 +1,25 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 export default function AdminProfile() {
     return(
-        <div className="card-body">
-            <div className="d-flex">
-                <p className="d-flex flex-column">
-                    <span className="text-bold text-lg">Admin Profile</span>
-                </p>
+        <div className="content-header">
+            <div className="container-fluid">
+                <div className="row mb-2">
+                    <div className="col-sm-6">
+                        <h1 className="m-0 text-dark">Admin Profile</h1>
+                    </div>
+                {/* /.col */}
+                    <div className="col-sm-6">
+                        <ol className="breadcrumb float-sm-right">
+                            <li className="breadcrumb-item"><a href="#">Home</a></li>
+                            <li className="breadcrumb-item active">Admin Profile</li>
+                        </ol>
+                    </div>
+                {/* /.col */}
+                </div>
+            {/* /.row */}
             </div>
+            {/* /.container-fluid */}
         </div>
     )
 }
